18efc7510d8bea42028f947437b69df719533a88
cuiyaning <cuiyaning@dp.tech>
2025-03-18 02:27:11 +0000

    [feat] update loss and model

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,4 @@
 from .model import train_epoch, validate_epoch, pkct_loss, decorate_torch_batch, process_net_targets
 from .unimol import UniMolModel
 from .pkmodel import UniPKModel, get_model_params
+from .loss import get_loss_fn, cal_all_losses
