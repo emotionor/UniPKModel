c6125fb0a490645bfda7aee47bcf1f7a7fdd2bea
cuiyaning <cuiyaning@dp.tech>
2025-05-22 06:31:43 +0000

    [feat] add admet pk pair train

--- data/__init__.py
+++ data/__init__.py
@@ -1,2 +1,3 @@
 from .conformer import ConformerGen
 from .dataset import SMILESDataset, read_data, load_or_create_dataset
+from .pairdataset import PairPKADMETDataset, load_or_create_pair_dataset, pair_batch_collate_fn
--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,6 @@
 from .model import train_epoch, validate_epoch, pkct_loss, decorate_torch_batch
-from .unimol import UniMolModel
+from .pairmodel import train_pair_epoch, validate_pair_epoch, pair_loss
+from .unimol import UniMolEncoder
 from .pkmodel import UniPKModel, get_model_params
 from .loss import get_loss_fn, cal_all_losses
+from .nnmodelzoo import TaskConditionedHead
