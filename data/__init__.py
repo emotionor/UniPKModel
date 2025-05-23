becd8ec5a2cb73e4e28603496c6e153b3eaf16d0
cuiyaning <cuiyaning@dp.tech>
2025-05-23 04:56:15 +0000

    [feat] update train admet only

--- data/__init__.py
+++ data/__init__.py
@@ -1,3 +1,4 @@
 from .conformer import ConformerGen
 from .dataset import SMILESDataset, read_data, load_or_create_dataset
 from .pairdataset import PairPKADMETDataset, load_or_create_pair_dataset, pair_batch_collate_fn
+from .admetdataset import load_or_create_admet_dataset, admet_batch_collate_fn
--- models/__init__.py
+++ models/__init__.py
@@ -4,3 +4,4 @@ from .unimol import UniMolEncoder
 from .pkmodel import UniPKModel, get_model_params
 from .loss import get_loss_fn, cal_all_losses
 from .nnmodelzoo import TaskConditionedHead
+from .admetmodel import train_admet_epoch, validate_admet_epoch, admet_loss
